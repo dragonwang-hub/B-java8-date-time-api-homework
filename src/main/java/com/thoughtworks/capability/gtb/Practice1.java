a03e2152c6ccb1d5fbfa013cb0d469f9ad0870b5
dragonwang-hub <915614879@qq.com>
2020-11-17 22:12:10 +0800

    [yunlong] feat: finished practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +11,9 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        LocalDate theNextLaborDay = LocalDate.of(2020, 5, 1)
+                .withYear(date.getMonth().getValue() < 5 ? date.getYear() : date.plusYears(1).getYear());
+        return  ChronoUnit.DAYS.between(date, theNextLaborDay);
+    }
 }
